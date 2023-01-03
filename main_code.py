576192d635194305d4a24f6bf9769285493e2e08
yiweichay <ywc19@ic.ac.uk>
2023-01-03 14:29:24 +0000

    Add laser position

--- main_code.py
+++ main_code.py
@@ -66,6 +66,7 @@ from matplotlib.widgets import Slider
 
 #Adjust the main plot to make room for the slider
 f, ax = plt.subplots(2,2,figsize=(10,10))
+f.tight_layout()
 plt.subplots_adjust(bottom = 0.25)
 ax_slider = f.add_axes([0.25, 0.1, 0.65, 0.03])
 imgslider = Slider(ax=ax_slider, label='Frame Number', valmin=0, valmax=10000, valinit=0, valstep=1)
@@ -91,14 +92,14 @@ def update(idx):
     img_new1 = transform(delta_x1, delta_y1, c1)
     img_new2 = transform(delta_x2, delta_y2, c2)
     img_new1crop = img_new1[70:130, 120:200] #crop image
-    im1 = ax[0,0].imshow(img_new1crop)
+    im1 = ax[0,0].imshow(img_new1)
     ax[0,0].grid(b=True, which='major', linestyle='-')
-    ax[0,0].set_title('Camera 1')
+    ax[0,0].set_title(f"Laser Position: {np.round(centroid[idx,0],2)},{np.round(centroid[idx,1],2)}")
 
     img_new2crop = img_new2[70:130, 120:200] #crop image
-    im2 = ax[1,0].imshow(img_new2crop)
+    im2 = ax[1,0].imshow(img_new2)
     ax[1,0].grid(b=True, which='major', linestyle='-')
-    ax[1,0].set_title('Camera 2')
+    ax[1,0].set_title(f"Laser Position: {np.round(centroid[idx,2],2)},{np.round(centroid[idx,3],2)}")
 
     '''
     #Check
@@ -123,9 +124,9 @@ def update(idx):
     #print(new.tolist())
     #print(new.max())
     #print(np.where(new == new.max()))
-    IR = ax[0,1].imshow(new, vmax=3)
+    IR = ax[0,1].imshow(R, vmax=3)
     ax[0,1].grid(b=True, which='major', linestyle='-')
-    ax[0,1].set_title('Intensity Ratio')
+    ax[0,1].set_title('Intensity Ratio: c2/c1')
     
     #IR = ax[0,1].imshow(R, vmax=3)
 
