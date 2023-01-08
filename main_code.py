5ee156b068e898970233e7c07301f09e5f2d6ebc
yiweichay <ywc19@ic.ac.uk>
2023-01-08 09:44:32 +0000

    minor changes

--- main_code.py
+++ main_code.py
@@ -72,13 +72,14 @@ center_y = 96
 
 #Add a slider
 from matplotlib.widgets import Slider
-
 #Adjust the main plot to make room for the slider
 f, ax = plt.subplots(2,3,figsize=(10,10))
 f.tight_layout()
 plt.subplots_adjust(bottom = 0.25)
 ax_slider = f.add_axes([0.25, 0.1, 0.65, 0.03])
 imgslider = Slider(ax=ax_slider, label='Frame Number', valmin=0, valmax=10000, valinit=0, valstep=1)
+
+#Add colorbar
 global colorbar_set #this is like volatile in C, for interrupts
 colorbar_set = False
 
@@ -103,26 +104,27 @@ def update(idx):
     img_new1crop = img_new1[70:130, 120:200] #crop image
     im1 = ax[0,0].imshow(img_new1)
     ax[0,0].grid(b=True, which='major', linestyle='-')
-    ax[0,0].set_title(f"Laser Position: {np.round(centroid[idx,0],2)},{np.round(centroid[idx,1],2)}")
+    ax[0,0].set_title(f"[c1] Laser Position: {np.round(centroid[idx,0],2)},{np.round(centroid[idx,1],2)}")
 
     img_new2crop = img_new2[70:130, 120:200] #crop image
     im2 = ax[1,0].imshow(img_new2)
     ax[1,0].grid(b=True, which='major', linestyle='-')
-    ax[1,0].set_title(f"Laser Position: {np.round(centroid[idx,2],2)},{np.round(centroid[idx,3],2)}")
+    ax[1,0].set_title(f"[c2] Laser Position: {np.round(centroid[idx,2],2)},{np.round(centroid[idx,3],2)}")
 
     
     #Check by multiplying the pixels
     M = np.multiply(img_new1, img_new2)
     print(M.max())
     print(np.where(M == M.max()))
+    #ax[0,2].clear()
     mul = ax[0,2].imshow(M)
     ax[0,2].grid(b=True, which='major', linestyle='-')
     ax[0,2].set_title('Multiplication of pixels c1xc2')
 
-    #Calculating intensity ratios
+
     #Denoise the aligned images
     img_new1, img_new2 = denoise(img_new1, img_new2, M)
-
+    #Calculating intensity ratios
     R = np.divide(img_new2, img_new1)
     R[np.isnan(R)] = 0
     R[np.isinf(R)] = 0
