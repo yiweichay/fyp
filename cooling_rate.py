7880fc3c5e4044dc7e41fb6de8b72ccb144ad1b2
yiweichay <ywc19@ic.ac.uk>
2023-02-21 10:57:56 +0000

    Print cooling rates multiple frames

--- cooling_rate.py
+++ cooling_rate.py
@@ -33,59 +33,65 @@ def mmToPixel(x, y):
     y = int(np.ceil(np.abs((y-(-71))/pix)) - 1)
     return x,y
 
-frame = int(input("Frame number:"))
-x = x_pos[frame]
-y = y_pos[frame]
-# x = float(input("Input x position: "))
-# y = float(input("Input y position: "))
-x,y = mmToPixel(x,y)
-print(x,y)
+# frame = int(input("Frame number:"))
+# x = x_pos[frame]
+# y = y_pos[frame]
+# # x = float(input("Input x position: "))
+# # y = float(input("Input y position: "))
+
+# print(x,y)
 
 
-temp_array = []
-time = []
+# temp_array = []
+# time = []
 
-with h5py.File(temp, 'r') as h:
-    for idx in list(h.keys())[frame-10:frame+50]:  
-        T_calculated = np.fliplr(np.rot90(h[idx][:], 3)) #rotate clockwise by 90 degrees
-        x_mu = x_pos[int(idx)]
-        y_mu = y_pos[int(idx)] 
-        globx, globy = mmToPixel(x_mu, y_mu)
+for frame in range(200, 400):
+    x = x_pos[frame]
+    y = y_pos[frame]
+    x,y = mmToPixel(x,y)
+    temp_array = []
+    time = []
+    with h5py.File(temp, 'r') as h:
+        for idx in list(h.keys())[frame-10:frame+50]:  
+            T_calculated = np.fliplr(np.rot90(h[idx][:], 3)) #rotate clockwise by 90 degrees
+            x_mu = x_pos[int(idx)]
+            y_mu = y_pos[int(idx)] 
+            globx, globy = mmToPixel(x_mu, y_mu)
 
-        globMat_copy = np.zeros((600,800))
-        if globMat_copy[globy-center_y:globy+center_y, globx-center_x:globx+center_x].shape == (192, 320):
-            globMat_copy[globy-center_y:globy+center_y, globx-center_x:globx+center_x] = T_calculated
-        else:
-            continue
-        #globMat = np.add(globMat, globMat_copy)
+            globMat_copy = np.zeros((600,800))
+            if globMat_copy[globy-center_y:globy+center_y, globx-center_x:globx+center_x].shape == (192, 320):
+                globMat_copy[globy-center_y:globy+center_y, globx-center_x:globx+center_x] = T_calculated
+            else:
+                continue
+            #globMat = np.add(globMat, globMat_copy)
 
-        temp_array.append(globMat_copy[y,x])
-        time.append(t[int(idx)])
-        #if int(idx) % 1000 == 0:
-        #    print(int(idx))
+            temp_array.append(globMat_copy[y,x])
+            time.append(t[int(idx)])
+            #if int(idx) % 1000 == 0:
+            #    print(int(idx))
 
-temp_array = np.array(temp_array)
-x = np.arange(len(temp_array))
-idx = np.nonzero(temp_array)
-#interp = interp1d(x[idx], temp_array[idx], fill_value='extrapolate')
-#temparray_new = interp(x)
+    temp_array = np.array(temp_array)
+    x = np.arange(len(temp_array))
+    idx = np.nonzero(temp_array)
+    #interp = interp1d(x[idx], temp_array[idx], fill_value='extrapolate')
+    #temparray_new = interp(x)
 
-time = np.array(time)
+    time = np.array(time)
 
-fig,ax = plt.subplots()
+    # fig,ax = plt.subplots()
 
-slope, intercept = np.polyfit(time, temp_array, 1) #Line of best fit
-print('Cooling Rate:', slope*10**-6, 'Intercept:', intercept)
+    slope, intercept = np.polyfit(time, temp_array, 1) #Line of best fit
+    print(frame, 'Cooling Rate:', slope*10**-6, 'Intercept:', intercept)
 
-plt.plot(time, temp_array, label='Cooling curve')
-plt.plot(time, slope*time+intercept, c='orange', label='Line of Best Fit')
-ax.legend()
+# plt.plot(time, temp_array, label='Cooling curve')
+# plt.plot(time, slope*time+intercept, c='orange', label='Line of Best Fit')
+# ax.legend()
        
 
-plt.xlabel('Time (s)')
-plt.ylabel('Temperature (K)')
-plt.title('Cooling Curve')
-plt.show()
+# plt.xlabel('Time (s)')
+# plt.ylabel('Temperature (K)')
+# plt.title('Cooling Curve')
+# plt.show()
 
 '''
 # --------------------------------------------------
